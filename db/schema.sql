c9df14026e51509afd1edcd808b2448cce3a15f1
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-11-06 21:03:38 -0500

    db info

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE rapapp_db;
+USE rapapp_db;
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	firstname varchar(50) NOT NULL,
+	lastname varchar (50) NOT NULL,
+	email varchar (50) NOT NULL,
+	month varchar (50) NOT NULL,
+	day INT (2) NOT NULL,
+	year INT (4) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE submission
+(
+	id int NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	user_input(20000) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE comments
+(
+	id int NOT NULL AUTO_INCREMENT,
+	submission_id INT  NOT NULL,
+	comments varchar (10000)
+	PRIMARY KEY (id)
+);
